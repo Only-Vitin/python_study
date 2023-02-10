867d313960ba9433af5375eba2650a6ab7ed265e
João Vitor <vitinhosantossilva123@gmail.com>
2023-02-10 12:51:31 -0300

    edit

--- hangman_game.py
+++ hangman_game.py
@@ -3,5 +3,24 @@ def jogar():
     print("Boas vindas à Forca", end="\n")
     print("_____________________________", end="\n\n")
 
+    palavra_secreta = "alface"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+
+        index = 1
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(chute,index))
+            index = index + 1
+
+        print("Jogando...")
+
+    print("Fim de Jogo")
+    
 if (__name__ == "__main__"):
     jogar()
