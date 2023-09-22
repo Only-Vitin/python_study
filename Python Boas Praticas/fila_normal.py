7a141ec9e2bb61f000103afbac4ed8ab52ba8001
Vitin <vitinhosantossilva123@gmail.com>
2023-09-21 23:21:58 -0300

    flake8

--- Python Boas Praticas/fila_normal.py
+++ Python Boas Praticas/fila_normal.py
@@ -3,15 +3,13 @@ from fila_base import FilaBase
 from constantes import CODIGO_NORMAL
 
 
-class FilaNormal(FilaBase):
-
+class filanormal(FilaBase):
     def __init__(self) -> None:
         super().__init__()
 
     def gera_senha_atual(self) -> None:
-        self.senha_atual = f'{CODIGO_NORMAL}{self.codigo}'
-        
-    #Para não dar erro
+        self.senha_atual = f"{CODIGO_NORMAL}{self.codigo}"
+
+    # Para não dar erro
     def estatistica(self, dia: str, agencia: str, flag: str):
         ...
-    
