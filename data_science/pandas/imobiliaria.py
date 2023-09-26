93273e2c72e298a3c46ceaa58394abc1788873bb
Vitin <vitinhosantossilva123@gmail.com>
2023-09-26 11:27:33 -0300

    adding plot with pandas and matplotlib

--- data_science/pandas/imobiliaria.py
+++ data_science/pandas/imobiliaria.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 
 URL = (
     "https://raw.githubusercontent.com/"
@@ -6,5 +7,11 @@ URL = (
     "main/base-de-dados/aluguel.csv"
 )
 dados = pd.read_csv(URL, sep=";")
-print(dados.head(10))
+print(dados.head(5))
+print()
 print(dados.info())
+print()
+
+media_tipo = dados.groupby("Tipo")[["Valor"]].mean().sort_values("Valor")
+media_tipo.plot(kind="barh", figsize=(14, 10), color="purple")
+plt.show()
