fe8b46f4dd56f212c6158a269e02aada742e98e2
Only-Vitin <vitinhosantossilva123@gmail.com>
2023-09-27 17:48:04 -0300

    project finished

---
+++ data_science/pandas/project_pandas_1/constantes.py
@@ -0,0 +1,8 @@
+IMOVEIS_COMERCIAIS= ['Conjunto Comercial/Sala', 
+                      'Prédio Inteiro', 'Loja/Salão', 
+                      'Galpão/Depósito/Armazém', 
+                      'Casa Comercial', 'Terreno Padrão',
+                      'Loja Shopping/ Ct Comercial',
+                      'Box/Garagem', 'Chácara',
+                      'Loteamento/Condomínio', 'Sítio',
+                      'Pousada/Chalé', 'Hotel', 'Indústria']
