6008cf9d53d8db4b5cd4f9f6c090fbfd81fdf6ba
Only-Vitin <vitinhosantossilva123@gmail.com>
2023-09-27 17:53:21 -0300

    deleted jupyter file

--- data_science/pandas/constantes.py
+++
@@ -1,8 +0,0 @@
-IMOVEIS_COMERCIAIS= ['Conjunto Comercial/Sala', 
-                      'Prédio Inteiro', 'Loja/Salão', 
-                      'Galpão/Depósito/Armazém', 
-                      'Casa Comercial', 'Terreno Padrão',
-                      'Loja Shopping/ Ct Comercial',
-                      'Box/Garagem', 'Chácara',
-                      'Loteamento/Condomínio', 'Sítio',
-                      'Pousada/Chalé', 'Hotel', 'Indústria']
